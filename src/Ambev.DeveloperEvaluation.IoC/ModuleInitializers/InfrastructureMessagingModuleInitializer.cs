9624c30fb4dc512031a822fac985e59d58c46fa7
Jose Ricardo Da Rocha Junior <jricardo@minsait.com>
2025-04-28 01:44:27 -0300

    Updated IOC project with modules initializers.

---
+++ src/Ambev.DeveloperEvaluation.IoC/ModuleInitializers/InfrastructureMessagingModuleInitializer.cs
@@ -0,0 +1,50 @@
+using Ambev.DeveloperEvaluation.Common.Messaging;
+using Ambev.DeveloperEvaluation.Messaging.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using RabbitMQ.Client;
+
+namespace Ambev.DeveloperEvaluation.IoC.ModuleInitializers;
+
+public class InfrastructureMessagingModuleInitializer : IModuleInitializer
+{
+    public void Initialize(WebApplicationBuilder builder)
+    {
+        if (builder.Environment.IsEnvironment("Testing"))
+        {
+            // Usa o publisher em memória durante os testes
+            builder.Services.AddSingleton<IEventPublisher, InMemoryProducer>();
+            return;
+        }
+
+        var hostName = builder.Configuration["RabbitMQ:HostName"];
+        var userName = builder.Configuration["RabbitMQ:UserName"];
+        var password = builder.Configuration["RabbitMQ:Password"];
+        var portString = builder.Configuration["RabbitMQ:Port"];
+
+        if (string.IsNullOrEmpty(hostName) ||
+            string.IsNullOrEmpty(userName) ||
+            string.IsNullOrEmpty(password) ||
+            string.IsNullOrEmpty(portString))
+        {
+            throw new InvalidOperationException("RabbitMQ configuration is missing.");
+        }
+
+        var port = int.Parse(portString);
+
+        var factory = new ConnectionFactory
+        {
+            HostName = hostName,
+            UserName = userName,
+            Password = password,
+            Port = port
+        };
+
+        var connection = factory.CreateConnection();
+
+        builder.Services.AddSingleton(connection);
+
+        builder.Services.AddSingleton<IEventPublisher, RabbitMqProducer>();
+    }
+}
